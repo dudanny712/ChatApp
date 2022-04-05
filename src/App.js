abc67cb104eeb431572e4eb6c34f5cf8f6b74076
dudanny712 <58696798+dudanny712@users.noreply.github.com>
2022-04-05 15:37:50 +0200

    Component updte

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-
+import {ChatEngine} from 'react-chat-engine';
+import ChatFeed from'./Component/ChatFeed'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <ChatEngine
+      height="100vh"
+      projectID= "5deed108-44f1-40d5-b18f-ad905990c99f"
+      userName= "Daniel"
+      userSecret= "1234567890"
+      renderChatFeed={(chatAppProps)=><ChatFeed {...chatAppProps}/>} 
+      onNewMessage={()=> new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
       
+      />
     </div>
   );
 }
